7415d8625b6fdd0cfaf32240152184497ff657d3
kassyn <kassyntec@gmail.com>
2015-05-20 11:53:43 -0300

    fixed indent size in utils js

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,23 @@
 ;(function(context, $) {
 
-  	'use strict';
+    'use strict';
 
-	Module.utils = {
-		toTitleCase : function(text) {
-		    text = text.replace(/(?:^|-)\w/g, function(match) {
-		        return match.toUpperCase();
-		    });
+    Module.utils = {
+        toTitleCase : function(text) {
+            text = text.replace(/(?:^|-)\w/g, function(match) {
+                return match.toUpperCase();
+            });
 
-		    return text.replace(/-/g, '');
-		},
+            return text.replace(/-/g, '');
+        },
 
-		toCamelCase : function(text) {
-		    text = text.replace(/(?:^|-)\w/g, function(match, index) {
-		        return ( !index ) ? match : match.toUpperCase();
-		    });
+        toCamelCase : function(text) {
+            text = text.replace(/(?:^|-)\w/g, function(match, index) {
+                return ( !index ) ? match : match.toUpperCase();
+            });
 
-		    return text.replace(/-/g, '');
-		}
-	};
+            return text.replace(/-/g, '');
+        }
+    };
 
 })( window, jQuery );
